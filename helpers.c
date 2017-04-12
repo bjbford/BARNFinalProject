b7e9644e56e9923b0ffe7270e0bd7c4a25bb95c1
Bradford <bjbford@iastate.edu>
2017-04-12 16:53:29 -0500

    Helpers.c
    Fixed errors in txtOutput(), compiles now using fwrite() instead of
    fprintf() and a for loop.

--- helpers.c
+++ helpers.c
@@ -33,17 +33,16 @@ void polar2Cart(int degree, float ir, float sonar, float *x, float *y){
 void txtOutput(float *x, float *y){
 	//Create .txt file called "dist.txt" to write into
 	FILE *file = fopen("dist.txt","w");
-	int i = 0, j = 0;
 
 	//write xCartesian Array values to "dist.txt" file
-	for(i=0; i < 92; i++)
-		fprintf(file, "%f\r\n", x[i]);
-	}
+	fwrite(x,sizeof(float),92,file);
+
 	//print a new line to space out x and y
+	char newLine[] = "\r\n";
+	fwrite(newLine,1,sizeof(newLine),file);
 
 	//write yCartesian Array values to "dist.txt" file
-	for(j=0; j < 92; j++){
-		fprintf(file, "%f\r\n", y[j]);
-	}
+	fwrite(y,sizeof(float),92,file);
+
 	fclose(file);
 }
