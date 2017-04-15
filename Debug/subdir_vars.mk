b44ee724796b95b3ffa4c3b6f40ec0b09b4c323c
andrewthai <andrew.thai@hotmail.com>
2017-04-15 13:52:39 -0500

    movement code

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
+SHELL = cmd.exe
+
 # Add inputs and outputs from these tool invocations to the build variables 
 CMD_SRCS += \
 ../tm4c123gh6pm.cmd 
@@ -19,19 +21,6 @@ C_SRCS += \
 ../tm4c123gh6pm_startup_ccs.c \
 ../uart.c 
 
-OBJS += \
-./WiFi.obj \
-./button.obj \
-./ir.obj \
-./lcd.obj \
-./main.obj \
-./open_interface.obj \
-./ping.obj \
-./servo.obj \
-./timer.obj \
-./tm4c123gh6pm_startup_ccs.obj \
-./uart.obj 
-
 C_DEPS += \
 ./WiFi.d \
 ./button.d \
@@ -45,18 +34,18 @@ C_DEPS += \
 ./tm4c123gh6pm_startup_ccs.d \
 ./uart.d 
 
-C_DEPS__QUOTED += \
-"WiFi.d" \
-"button.d" \
-"ir.d" \
-"lcd.d" \
-"main.d" \
-"open_interface.d" \
-"ping.d" \
-"servo.d" \
-"timer.d" \
-"tm4c123gh6pm_startup_ccs.d" \
-"uart.d" 
+OBJS += \
+./WiFi.obj \
+./button.obj \
+./ir.obj \
+./lcd.obj \
+./main.obj \
+./open_interface.obj \
+./ping.obj \
+./servo.obj \
+./timer.obj \
+./tm4c123gh6pm_startup_ccs.obj \
+./uart.obj 
 
 OBJS__QUOTED += \
 "WiFi.obj" \
@@ -70,6 +59,19 @@ OBJS__QUOTED += \
 "timer.obj" \
 "tm4c123gh6pm_startup_ccs.obj" \
 "uart.obj" 
+
+C_DEPS__QUOTED += \
+"WiFi.d" \
+"button.d" \
+"ir.d" \
+"lcd.d" \
+"main.d" \
+"open_interface.d" \
+"ping.d" \
+"servo.d" \
+"timer.d" \
+"tm4c123gh6pm_startup_ccs.d" \
+"uart.d" 
 
 C_SRCS__QUOTED += \
 "../WiFi.c" \
--- main.c
+++ main.c
@@ -9,7 +9,16 @@
 #include "ir.h"
 #include "button.h"
 
+
 void init_All();
+void move_roomba() ;
+void move_forward(oi_t *sensor, int centimeters);
+void move_backwards(oi_t *sensor, int centimeters) ;
+void turn_counterClockwise(oi_t *sensor, int degrees);
+void turn_clockwise(oi_t *sensor, int degrees) ;
+int checkBumper() ;
+
+
 
 /*
  * main.c
@@ -17,14 +26,26 @@ void init_All();
 int main(void) {
 	init_All();
 	
-	return 0;
+
+	oi_t *sensor_data = oi_alloc() ;
+	oi_init(sensor_data) ;
+
+
+
+	while (1)
+	{
+	    move_roomba() ;
+	}
+
+
+
 }
 
 /**
  *
  */
 void init_All(){
-	WiFi_start("password");
+//	WiFi_start("password");
 	uart_init();
 	lcd_init();
 	button_init();
@@ -33,4 +54,135 @@ void init_All(){
 	servo_init();
 	oi_t *sensor_data = oi_alloc();
 	oi_init(sensor_data);
+
 }
+
+/**
+ *
+ */
+void move_roomba ()
+{
+       oi_t *sensor_data = oi_alloc();
+       oi_init(sensor_data);
+       char input ;
+
+//        scanf("%c", &input) ;
+        input = uart_receive();
+        //forwards
+        if (input == 'u')
+        {
+            move_forward(sensor_data, 100); //move forward
+
+        }
+        //backwards
+        else if (input == 'b')
+        {
+            move_backwards(sensor_data, -100); //backwards
+        }
+
+        //rotates clockwise
+        else if (input == 'd')
+        {
+            turn_clockwise(sensor_data, 5) ;//rotate 90 degrees
+
+        }
+
+        //rotates counterclockwise
+        else if (input == 'a')
+        {
+            turn_counterClockwise(sensor_data, 5) ;
+        }
+
+        //scan surroundings
+        else if (input == 'p')
+        {
+
+        }
+        oi_free(sensor_data) ;
+}
+
+void move_forward(oi_t *sensor, int centimeters){
+
+
+    oi_setWheels(100,100) ; //move forward
+
+    int sum = 0 ;
+
+    while (sum < centimeters)
+    {
+
+        oi_update(sensor);
+        sum += sensor->distance;
+        timer_waitMillis(100);
+    }
+
+    oi_setWheels(0,0); //stop wheels
+    oi_free(sensor) ;
+
+}
+
+void move_backwards(oi_t *sensor, int centimeters)
+{
+//    oi_t *sensor_data = oi_alloc() ;
+//    oi_init(sensor_data) ;
+
+    oi_setWheels(-100, -100) ; //move backward; half speed
+
+    int sum = 0 ;
+
+    while (sum > centimeters)
+    {
+        oi_update(sensor) ;
+
+        sum += sensor->distance;
+        timer_waitMillis(1);
+
+    }
+
+    oi_setWheels(0,0) ; //stop wheels
+    oi_free(sensor) ;
+}
+
+void turn_counterClockwise(oi_t *sensor, int degrees){
+//    oi_t *sensor_data = oi_alloc();
+//    oi_init(sensor_data);
+
+    oi_setWheels(100, -100);
+//    int initial_angle = sensor->angle;
+//    int angle = 0;
+//    while(abs(initial_angle-angle) < degrees){
+//        oi_update(sensor);
+//        angle += sensor->angle;
+//    }
+    timer_waitMillis(degrees*22);
+    oi_setWheels(0, 0); //stop wheels
+    oi_free(sensor) ;
+
+}
+
+void turn_clockwise(oi_t *sensor, int degrees){
+//    oi_t *sensor_data = oi_alloc();
+//    oi_init(sensor_data);
+
+    oi_setWheels(-100, 100);
+//    int initial_angle = sensor->angle;
+//    int angle = 0;
+//    while(abs(initial_angle-angle) < degrees){
+//        oi_update(sensor);
+//        angle += sensor->angle;
+//    }
+    timer_waitMillis(degrees*22);
+    oi_setWheels(0, 0); //stop wheels
+    oi_free(sensor) ;
+}
+
+
+void scan()
+{
+
+}
+
+
+
+
+
